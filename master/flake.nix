b13a47d036451df5062a26f948f7c957e6c37450
hugosenari <hugosenari@gmail.com>
2022-03-12 02:26:18 -0300

    chore: re index owo

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''OwO text convewtew fow Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-owo-master.flake = false;
+  inputs.src-owo-master.owner = "lmariscal";
+  inputs.src-owo-master.ref   = "refs/heads/master";
+  inputs.src-owo-master.repo  = "owo";
+  inputs.src-owo-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-owo-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-owo-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
